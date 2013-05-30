96b6c05289102fcece79309b1d1c2eb8992a9e8e
Benjamin P. Jung <headcr4sh@gmail.com>
2013-05-30 09:23:29 +0200

    Last preparations for initial release to OSS Sonatype Maven repo.

--- src/main/java/com/cathive/fonts/roboto/RobotoFonts.java
+++ src/main/java/com/cathive/fonts/roboto/RobotoFonts.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2012, The Cat Hive Developers
+ * Copyright (c) 2013, The Cat Hive Developers
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package com.cathive.fonts.roboto;
 
 /**
- * A bunch of String contants that define the different font names
+ * A bunch of String constants that define the different font names
  * that can be used.
  * 
  * @author Benjamin P. Jung
